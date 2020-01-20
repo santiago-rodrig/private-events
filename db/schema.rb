882ea0023ab51b878d98346a3d693ec165cd4925
Santiago <santo1996.29@gmail.com>
2020-01-20 18:26:23 -0400

    add name column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200120221534) do
+ActiveRecord::Schema.define(version: 20200120222552) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
