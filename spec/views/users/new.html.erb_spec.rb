6286f9c90bf5412c86c8ca655cd14ef2d3b55221
Santiago <santo1996.29@gmail.com>
2020-01-20 18:51:00 -0400

    ignore examples.txt please, new template specs

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "users/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'renders a form with the right action' do
+    render
+    expect(rendered).to match(
+      Regexp.new(
+        '.*<form.*action=/users.*',
+        1 | 4
+      )
+    )
+  end
+
+  it 'renders a text input inside the form' do
+    render
+    expect(rendered).to match(
+      Regexp.new(
+        '.*<form.*>.*<input.*type=\'text\'.*',
+        1 | 4
+      )
+    )
+  end
+
+  it 'renders a submit button inside the form' do
+    render
+    expect(rendered).to match(
+      Regexp.new(
+        '.*<form.*>.*<input.*type=\'submit\'.*',
+        1 | 4
+      )
+    )
+  end
 end
