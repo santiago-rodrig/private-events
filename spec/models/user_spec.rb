88569b58346994e246882de1372a135a36fcf2b7
Santiago <santo1996.29@gmail.com>
2020-01-20 18:30:27 -0400

    routes for users resource

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe User, type: :model do
   context 'creating a user' do
     it 'creates a user' do
       expect { User.create(name: 'bob') }.not_to raise_error
+      count = User.count
+      User.create(name: 'jen')
+      expect(User.count).to eq(count + 1)
     end
   end
 end
