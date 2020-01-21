a6faae7e0c978031a0126453e7e818aeaf305959
Santiago <santo1996.29@gmail.com>
2020-01-21 12:33:23 -0400

    migrated pending migrations in dev and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200121161428) do
+ActiveRecord::Schema.define(version: 20200121162843) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
