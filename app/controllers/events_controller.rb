0648e3f0291a27c72815c2c3e0694e9116c81431
Santiago <santo1996.29@gmail.com>
2020-01-21 13:48:11 -0400

    create event specs passing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,21 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def create
+    @user = User.find(session[:user_id])
+    @event = @user.events.build(event_params)
+
+    if @event.save
+      redirect_to events_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date)
+  end
 end
