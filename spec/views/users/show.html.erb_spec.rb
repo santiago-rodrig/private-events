f55c7c9f3fd182c733611fe2994573ee1a852b4f
Santiago <santo1996.29@gmail.com>
2020-01-21 09:57:07 -0400

    specs for users show template

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "users/show.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(name: 'bob')
+    assign(:user, @user)
+    render
+  end
+
+  it 'displays the name of the user' do
+    expect(rendered).to match(
+      Regexp.new(
+        ".*#{@user.name}.*",
+        1 | 4
+      )
+    )
+  end
 end
