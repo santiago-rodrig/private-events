5715134af81a9cf5d77d518956e53e295228fc7d
Santiago <santo1996.29@gmail.com>
2020-01-21 11:10:34 -0400

    sessions controller specs for new action passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
+  get 'login', to: 'sessions#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
