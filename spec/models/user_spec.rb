abfb610876699f89bac5bca17e30c156fd888f3e
Santiago <santo1996.29@gmail.com>
2020-01-21 12:40:38 -0400

    user-event association specs passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'creating a user' do
-    it 'creates a user' do
-      expect { User.create(name: 'bob') }.not_to raise_error
-      count = User.count
-      User.create(name: 'jen')
-      expect(User.count).to eq(count + 1)
-    end
+  it 'creates a user' do
+    expect { User.create(name: 'bob') }.not_to raise_error
+  end
+
+  it 'add a new user' do
+    count = User.count
+    User.create(name: 'jen')
+    expect(User.count).to eq(count + 1)
+  end
+
+  it 'has many events' do
+    user = User.create(name: 'jen')
+
+    event = Event.create(
+      name: 'party', date: (Time.now + 3600).to_date, user_id: user.id
+    )
+
+    expect(user.respond_to?(:events)).to be_truthy
+    expect(user.events).to include(event)
   end
 end
