e50a06cac952b89418a3899989cb225341604656
Santiago <santo1996.29@gmail.com>
2020-01-21 17:02:50 -0400

    association attendee-attended-events functional

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Attendance, type: :model do
   before do
     @user = User.create(name: 'bob')
-    @event = Event.create(description: 'hugue party')
+    @event = @user.events.create(description: 'hugue party')
     @attendance = Attendance.create(attendee_id: @user.id, attended_event_id: @event.id)
   end
 
@@ -13,7 +13,7 @@ RSpec.describe Attendance, type: :model do
   end
 
   it 'belongs to an attended_event' do
-    expect(@attendance).to be_respond_yo(:attended_event)
+    expect(@attendance).to be_respond_to(:attended_event)
     expect(@attendance.attended_event).to eq(@event)
   end
 end
