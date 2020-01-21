9631c035b6e177e74622c0ae3e48d9a2ba2b6f1e
Santiago <santo1996.29@gmail.com>
2020-01-21 12:31:58 -0400

    wrote migration for adding user_id to events

--- db/migrate/20200121162843_add_user_id_to_events.rb
+++ db/migrate/20200121162843_add_user_id_to_events.rb
@@ -1,4 +1,9 @@
 class AddUserIdToEvents < ActiveRecord::Migration[5.1]
   def change
+    change_table :events do |t|
+      t.references :user
+    end
+
+    add_index :events, :user_id, unique: true
   end
 end
