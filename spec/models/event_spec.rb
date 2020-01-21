9bd48aeadb926378d22275dcf0917d793149dc48
Santiago <santo1996.29@gmail.com>
2020-01-21 12:20:02 -0400

    event model specs and also passing

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates a new event successfully' do
+    expect do
+      Event.create(name: 'event', date: (Time.now + 3600).to_date)
+    end.not_to raise_error
+  end
 end
