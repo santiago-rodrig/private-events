3e6f1481a6abfe6c9d5052c0ad37c498aeef73bb
Santiago <santo1996.29@gmail.com>
2020-01-21 18:40:11 -0400

    past and future event model specs passing

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :attendances, foreign_key: 'attended_event_id'
   has_many :attendees, through: :attendances, source: :attendee
+
+  def self.past
+    self.where('date < ?', Time.now.to_date)
+  end
+
+  def self.future
+    self.where('date > ?', Time.now.to_date)
+  end
 end
