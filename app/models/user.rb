6844c9f2e4383f36997036ffe05505ee692b6444
Santiago <santo1996.29@gmail.com>
2020-01-21 17:42:55 -0400

    upcoming and past attended events specs passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :events
   has_many :attendances, foreign_key: 'attendee_id'
   has_many :attended_events, through: :attendances, source: :attended_event
+
+  def upcoming_attended_events
+    attended_events.where('date > ?', Time.now.to_date)
+  end
+
+  def past_attended_events
+    attended_events.where('date < ?', Time.now.to_date)
+  end
 end
