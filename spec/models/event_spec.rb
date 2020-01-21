7274d61a471d99881a45c5455e04e4e8c663bb1d
Santiago <santo1996.29@gmail.com>
2020-01-21 12:22:07 -0400

    more event specs

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Event, type: :model do
       Event.create(name: 'event', date: (Time.now + 3600).to_date)
     end.not_to raise_error
   end
+
+  it 'adds a new event' do
+    count = Event.count
+    Event.create(name: 'event', date: (Time.now + 3600).to_date)
+    expect(Event.count).to eq(count + 1)
+  end
 end
