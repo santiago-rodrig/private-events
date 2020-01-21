ffc0e51467fe34a846cff950a73925317addad7d
Santiago <santo1996.29@gmail.com>
2020-01-21 11:42:31 -0400

    login action sessions controller specs passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,13 @@
 class SessionsController < ApplicationController
   def new; end
+
+  def login
+    @user = User.find_by(name: params[:name])
+
+    if @user
+      session[:user_id] = @user.id
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
