7403aa286c83248cdca1149c32ba64a8fef38b53
Santiago <santo1996.29@gmail.com>
2020-01-21 11:07:42 -0400

    specs for sessions controller new action

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe 'GET #new' do
+    before do
+      get :new
+    end
 
+    it 'returns HTTP success' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST #login' do
+  end
+
+  describe 'DELETE #logout' do
+  end
 end
