286b3c4e6e67351930e85cb39bc9f0bcca8fe7aa
Santiago <santo1996.29@gmail.com>
2020-01-21 15:11:51 -0400

    index template events specs

---
+++ spec/views/events/index.html.erb_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'events/index.html.erb', type: :view do
+  before do
+    Event.create([
+      { name: 'party', date: (Time.now + 3600).to_date },
+      { name: 'beach party', date: (Time.now + 7200).to_date },
+      { name: 'birthday party', date: (Time.now + 10800).to_date }
+    ])
+
+    render
+  end
+
+  it 'displays all the event names' do
+    Event.all.each do |event|
+      expect(rendered).to match(
+        Regexp.new(
+          ".*#{event.name}.*",
+          1 | 4
+        )
+      )
+    end
+  end
+
+  context 'user logged in' do
+    before do
+      @user = User.create(name: 'bob')
+      controller.session[:user_id] = @user.id
+      render template: 'events/index', layout: 'layouts/application'
+    end
+
+    it 'displays the name of the logged user at the top' do
+      expect(rendered).to match(
+        Regexp.new(
+          ".*<header.*>.*#{@user.name}.*</header>.*",
+          1 | 4
+        )
+      )
+    end
+  end
+end
