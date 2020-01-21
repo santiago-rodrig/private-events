cf7d3f602c408cd50c094e7ce0161de21ca8cf0d
Santiago <santo1996.29@gmail.com>
2020-01-21 12:27:33 -0400

    event-user association specs in events

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,15 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  before do
+    @user = User.create(name: 'bob')
+  end
+
+  it 'belongs to a user' do
+    event = Event.create(
+      name: 'event', date: (Time.now + 3600).to_date, user_id: @user.id
+    )
+
+    expect(event.respond_to(:user))
+    expect(event.user).to eq(@user)
+  end
+
   it 'creates a new event successfully' do
     expect do
-      Event.create(name: 'event', date: (Time.now + 3600).to_date)
+      Event.create(
+        name: 'event', date: (Time.now + 3600).to_date, user_id: @user.id
+      )
     end.not_to raise_error
   end
 
   it 'adds a new event' do
     count = Event.count
-    Event.create(name: 'event', date: (Time.now + 3600).to_date)
+    Event.create(
+      name: 'event', date: (Time.now + 3600).to_date, user_id: @user.id
+    )
     expect(Event.count).to eq(count + 1)
   end
 end
