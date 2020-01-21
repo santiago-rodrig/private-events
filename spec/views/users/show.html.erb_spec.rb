78da7a4b0473c914662ac1ec5b68f4bd1880687f
Santiago <santo1996.29@gmail.com>
2020-01-21 12:11:44 -0400

    name of current user at top show view specs passed

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -15,4 +15,22 @@ RSpec.describe "users/show.html.erb", type: :view do
       )
     )
   end
+
+  context 'the user is signed in' do
+    before do
+      @user = User.create(name: 'bob')
+      controller.session[:user_id] = @user.id
+      assign(:user, @user)
+      render template: 'users/new', layout: 'layouts/application'
+    end
+
+    it 'displays the name of the user at the top' do
+      expect(rendered).to match(
+        Regexp.new(
+          ".*<header.*>.*#{@user.name}.*</header>.*",
+          1 | 4
+        )
+      )
+    end
+  end
 end
