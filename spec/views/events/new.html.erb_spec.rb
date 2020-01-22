45dad426b4df3cab8930892f385ca6fae2b62849
Santiago <santo1996.29@gmail.com>
2020-01-22 11:52:21 -0400

    specs for date field in event creation

--- spec/views/events/new.html.erb_spec.rb
+++ spec/views/events/new.html.erb_spec.rb
@@ -25,6 +25,15 @@ RSpec.describe 'events/new.html.erb', type: :view do
     )
   end
 
+  it 'displays a date field for the event date' do
+    expect(rendered).to match(
+      Regexp.new(
+        '.*<form.*>.*<input.*type="date".*>.*</form>.*',
+        1 | 4
+      )
+    )
+  end
+
   it 'displays a submit button' do
     expect(rendered).to match(
       Regexp.new(
