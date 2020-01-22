beb57ee211859d07eacb4e78ee8428806231fce4
Santiago <santo1996.29@gmail.com>
2020-01-22 18:19:43 -0400

    specs for user show page

--- spec/system/show_users_spec.rb
+++ spec/system/show_users_spec.rb
@@ -2,8 +2,30 @@ require 'rails_helper'
 
 RSpec.describe "ShowUsers", type: :system do
   before do
-    driven_by(:rack_test)
+    driven_by(:selenium_chrome)
+    @user = User.create(name: 'bob')
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'shows a user profile' do
+    visit(user_path(@user))
+    expect(page).to have_current_path(user_path(@user))
+    expect(page).to have_content(@user.name)
+    expect(page).not_to have_content('Invitations')
+    expect(page).to have_content('All attended events')
+    expect(page).to have_content('Past attended events')
+    expect(page).to have_content('Upcoming events to attend')
+  end
+
+  it 'shows invitations if is the same user' do
+    visit('/login')
+    fill_in('Name', with: 'bob')
+    find('input[type="submit"]').click
+    visit(user_path(@user))
+    expect(page).to have_current_path(user_path(@user))
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content('Invitations')
+    expect(page).to have_content('All attended events')
+    expect(page).to have_content('Past attended events')
+    expect(page).to have_content('Upcoming events to attend')
+  end
 end
