3107a02c5927a7ef733541e6d96f18f7a3a14ae8
Santiago <santo1996.29@gmail.com>
2020-01-22 11:07:03 -0400

    links to move around at top

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#login'
   delete 'logout', to: 'sessions#logout'
   resources :events, only: [:new, :create, :show, :index]
+  root 'events#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
