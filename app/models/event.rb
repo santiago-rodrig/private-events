5bb16502a3799fc71caf0c16c2fde4be9c471570
Santiago <santo1996.29@gmail.com>
2020-01-22 12:54:37 -0400

    specs for invited-inviting_event association pass

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :attendances, foreign_key: 'attended_event_id'
   has_many :attendees, through: :attendances, source: :attendee
+  has_many :invitations, foreign_key: 'inviting_event_id'
+  has_many :inviteds, through: :invitations, source: :invited
   scope :past, -> { where('date < ?', Time.now.to_date) }
   scope :future, -> { where('date > ?', Time.now.to_date) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :events
   has_many :attendances, foreign_key: 'attendee_id'
   has_many :attended_events, through: :attendances, source: :attended_event
+  has_many :invitations, foreign_key: 'invited_id'
+  has_many :inviting_events, through: :invitations, source: :inviting_event
 
   def upcoming_attended_events
     attended_events.where('date > ?', Time.now.to_date)
