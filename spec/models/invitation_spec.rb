8688c7664c77774d82abaf75eae477cd8761609a
Santiago <santo1996.29@gmail.com>
2020-01-22 12:41:10 -0400

    invitation model association specs

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(name: 'bob')
+    @other_user = User.create(name: 'jen')
+
+    @event = @other_user.events.create(
+      description: 'party')
+
+    @invitation = Invitation.create(
+      invited_id: @user.id, inviting_event_id: @event.id)
+  end
+
+  it 'belongs to an invited' do
+    expect(@invitation).to be_respond_to(:invited)
+    expect(@invitation.invited).to eq(@user)
+  end
+
+  it 'belongs to an inviting_event' do
+    expect(@invitation).to be_respond_to(:inviting_event)
+    expect(@invitation.inviting_event).to eq(@event)
+  end
 end
