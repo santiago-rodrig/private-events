01b23035f066fac06485a6d457a645103c123581
Santiago <santo1996.29@gmail.com>
2020-01-22 18:30:14 -0400

    specs for login with capybara

--- spec/system/log_ins_spec.rb
+++ spec/system/log_ins_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "LogIns", type: :system do
   before do
-    driven_by(:rack_test)
+    driven_by(:selenium_chrome)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'logs in a user' do
+    @user = User.create(name: 'bob')
+    visit(login_path)
+    fill_in('Name', with: 'bob')
+    find('input[type="submit"]').click
+    expect(page).to have_current_path('/')
+    expect(page).to have_content('Logout')
+    expect(page).to have_content('Create event')
+    expect(page).to have_content(@user.name)
+  end
 end
