ebf5266deccee3934f0cde51862f9be2a53b4431
Santiago <santo1996.29@gmail.com>
2020-01-23 11:02:23 -0400

    removed unnecessary line in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,8 +11,6 @@ require 'capybara/rspec'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
